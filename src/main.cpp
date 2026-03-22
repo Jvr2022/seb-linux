dc662fe2e65ad1b5160fed2aee8236d17353af58
Jouke van Dam <jouke@tayami.net>
2026-03-22 13:12:21 +0100

    Add linux VT anticheat

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,81 @@
 #include <QLineEdit>
 #include <QTextStream>
 #include <QUrl>
-#include <QFileInfo>
+#include <QProcess>
+#include <QProcessEnvironment>
+#include <QTemporaryFile>
+#include <QDir>
 
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/vt.h>
+#include <linux/kd.h>
+#include <signal.h>
 namespace {
+
+int g_tty0_fd = -1;
+int g_new_tty_fd = -1;
+
+void cleanup_vt_and_exit() {
+    if (g_new_tty_fd >= 0) {
+        ioctl(g_new_tty_fd, KDSETMODE, KD_TEXT);
+    }
+    if (g_tty0_fd >= 0) {
+        ioctl(g_tty0_fd, VT_ACTIVATE, 1);
+        ioctl(g_tty0_fd, VT_WAITACTIVE, 1);
+    }
+    if (g_new_tty_fd >= 0) close(g_new_tty_fd);
+    if (g_tty0_fd >= 0) close(g_tty0_fd);
+}
+
+void barebones_sig_handler(int signum) {
+    cleanup_vt_and_exit();
+    if (signum == SIGSEGV || signum == SIGABRT || signum == SIGFPE || signum == SIGILL) {
+        signal(signum, SIG_DFL);
+        raise(signum);
+    } else {
+        _exit(1);
+    }
+}
+
+void setup_barebones_vt() {
+    signal(SIGINT, barebones_sig_handler);
+    signal(SIGTERM, barebones_sig_handler);
+    signal(SIGSEGV, barebones_sig_handler);
+    signal(SIGABRT, barebones_sig_handler);
+    signal(SIGILL, barebones_sig_handler);
+    signal(SIGFPE, barebones_sig_handler);
+
+    g_tty0_fd = open("/dev/tty0", O_RDWR);
+    if (g_tty0_fd < 0) {
+        qWarning() << "Could not open /dev/tty0. Did you run the app as root?";
+        return;
+    }
+
+    int free_vt = -1;
+    if (ioctl(g_tty0_fd, VT_OPENQRY, &free_vt) < 0 || free_vt == -1) {
+        qWarning() << "Could not find a free VT.";
+        return;
+    }
+
+    char vt_name[20];
+    snprintf(vt_name, sizeof(vt_name), "/dev/tty%d", free_vt);
+    g_new_tty_fd = open(vt_name, O_RDWR);
+    if (g_new_tty_fd < 0) {
+        qWarning() << "Could not open VT" << vt_name;
+        return;
+    }
+
+    ioctl(g_tty0_fd, VT_ACTIVATE, free_vt);
+    ioctl(g_tty0_fd, VT_WAITACTIVE, free_vt);
+
+    if (ioctl(g_new_tty_fd, KDSETMODE, KD_GRAPHICS) < 0) {
+        qWarning() << "Error setting graphics mode on VT" << free_vt;
+    }
+
+    atexit(cleanup_vt_and_exit);
+}
 
 QString findConfigPath(int argc, char *argv[])
 {
@@ -44,6 +116,15 @@ void applyEarlyEnvironment(int argc, char *argv[])
         const seb::LoadResult loaded = seb::loadSettingsFromFile(configPath);
         if (loaded.ok) {
             settings = loaded.settings;
+        }
+    }
+    for (int i = 0; i <= argc; ++i) { // we have to use a loop because the command line parser was not yet loaded.
+        if (QString::fromLocal8Bit(argv[i]) == QStringLiteral("--anti-cheat")) {
+            setup_barebones_vt();
+            qputenv("QT_QPA_PLATFORM", "linuxfb");
+            qputenv("QTWEBENGINE_CHROMIUM_FLAGS", "--no-sandbox");
+            qputenv("QT_QUICK_BACKEND", "software");
+            break;
         }
     }
 
@@ -82,7 +163,9 @@ void applyCommandLineOverrides(const QCommandLineParser &parser, seb::SebSetting
     if (parser.isSet("windowed")) {
         settings.browser.mainWindow.fullScreenMode = false;
     }
-
+    if (parser.isSet("fullscreen")){
+        settings.browser.mainWindow.fullScreenMode = true;
+    }
     if (parser.isSet("always-on-top")) {
         settings.browser.mainWindow.alwaysOnTop = true;
     }
@@ -109,7 +192,8 @@ int main(int argc, char *argv[])
     applyEarlyEnvironment(argc, argv);
 
     QApplication app(argc, argv);
-    const QIcon appIcon(QStringLiteral(":/assets/icons/safe-exam-browser.png"));
+    
+     const QIcon appIcon(QStringLiteral(":/assets/icons/safe-exam-browser.png"));
     app.setWindowIcon(appIcon);
     app.setDesktopFileName(QStringLiteral("safe-exam-browser"));
     QCoreApplication::setApplicationName(QStringLiteral("Safe Exam Browser"));
@@ -142,6 +226,7 @@ int main(int argc, char *argv[])
         QStringLiteral("allow-devtools"),
         QStringLiteral("Enable the developer tools shortcut (F12).")));
     parser.addOption(QCommandLineOption(QStringLiteral("windowed"), QStringLiteral("Force the main window to stay windowed.")));
+    parser.addOption(QCommandLineOption(QStringLiteral("fullscreen"), QStringLiteral("Force the main window to be fullscreen.")));
     parser.addOption(QCommandLineOption(QStringLiteral("always-on-top"), QStringLiteral("Keep the main window above other windows.")));
     parser.addOption(QCommandLineOption(
         QStringLiteral("disable-minimize"),
@@ -149,6 +234,9 @@ int main(int argc, char *argv[])
     parser.addOption(QCommandLineOption(
         QStringLiteral("disable-quit"),
         QStringLiteral("Disable manual termination even if the configuration allows it.")));
+    parser.addOption(QCommandLineOption(
+        QStringLiteral("anti-cheat"),
+        QStringLiteral("Enable anticheat mode.")));
 #ifdef QT_DEBUG
     parser.addOption(QCommandLineOption(
         QStringList{QStringLiteral("i"), QStringLiteral("inject")},
@@ -165,9 +253,27 @@ int main(int argc, char *argv[])
         ? parser.value("config")
         : (parser.positionalArguments().isEmpty() ? QString() : parser.positionalArguments().constFirst());
 
+    QString userPassword;
+    bool usedPassword = false;
+
     QStringList warnings;
     if (!resource.isEmpty()) {
-        const seb::ResourceLoadResult loaded = seb::loadSettingsFromResource(resource, [](bool hashed) {
+        const seb::ResourceLoadResult loaded = seb::loadSettingsFromResource(resource, [&userPassword, &usedPassword](bool hashed) {
+            if (qEnvironmentVariableIsSet("SEB_PASSWORD_FILE")) {
+                QString tempFilePath = QString::fromUtf8(qgetenv("SEB_PASSWORD_FILE"));
+                QFile file(tempFilePath);
+                if (file.open(QIODevice::ReadOnly)) {
+                    QByteArray data = file.readAll();
+                    userPassword = QString::fromUtf8(data);
+                    if (userPassword.endsWith('\n')) {
+                        userPassword.chop(1);
+                    }
+                    file.remove(); // Unlink immediately
+                    usedPassword = true;
+                    return userPassword;
+                }
+            }
+
             bool accepted = false;
             const QString password = QInputDialog::getText(
                 nullptr,
@@ -178,6 +284,10 @@ int main(int argc, char *argv[])
                 QLineEdit::Password,
                 QString(),
                 &accepted);
+            if (accepted) {
+                userPassword = password;
+                usedPassword = true;
+            }
             return accepted ? password : QString();
         });
         if (!loaded.ok) {
@@ -190,6 +300,44 @@ int main(int argc, char *argv[])
     }
 
     applyCommandLineOverrides(parser, settings);
+
+    if (!parser.isSet("anti-cheat")) {
+        if (settings.browser.mainWindow.fullScreenMode && settings.browser.mainWindow.alwaysOnTop) {
+            QStringList args = QCoreApplication::arguments();
+            args.removeFirst(); // Remove executable path
+            if (!args.contains(QStringLiteral("--anti-cheat"))) {
+                args.prepend(QStringLiteral("--anti-cheat"));
+            }
+            
+            QProcess child;
+            child.setProgram(QStringLiteral("pkexec"));
+            
+            QStringList pkexecArgs;
+            QTemporaryFile *tempFile = nullptr;
+            if (usedPassword) {
+                tempFile = new QTemporaryFile(QDir::tempPath() + QStringLiteral("/seb-pass-XXXXXX"));
+                tempFile->setAutoRemove(false);
+                if (tempFile->open()) {
+                    tempFile->write(userPassword.toUtf8());
+                    tempFile->close();
+                    
+                    pkexecArgs << QStringLiteral("env");
+                    pkexecArgs << (QStringLiteral("SEB_PASSWORD_FILE=") + tempFile->fileName());
+                }
+            }
+            
+            pkexecArgs << QCoreApplication::applicationFilePath();
+            pkexecArgs << args;
+            
+            child.setArguments(pkexecArgs);
+            
+            if (child.startDetached()) {
+                return 0;
+            }
+            delete tempFile;
+            return 1;
+        }
+    }
 
     AppController controller;
     QString launchError;
