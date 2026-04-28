81d4b9ee087c7b81c25d1a61cc8b96202d4d30e6
tectrixdev <joranhennion@duck.com>
2026-04-28 20:50:06 +0200

    Change directory, output AppImage.

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -13,39 +13,21 @@ mkdir -p "${BUILD_DIR}" "${STAGE_DIR}" "${ARTIFACT_DIR}"
 rm -rf "${BUILD_DIR:?}/"* "${STAGE_DIR:?}/"*
 
 # TODO: Consistency
-# TODO: Move appdir to build-stage, edit all paths
 
 # Build AppImages:
-qmake6 CONFIG+=force_qtwebengine INSTALL_ROOT=${PROJECT_DIR}/AppDir
+qmake6 CONFIG+=force_qtwebengine INSTALL_ROOT=${STAGE_DIR}/AppDir
 make -j$(nproc)
-make install INSTALL_ROOT=${PROJECT_DIR}/AppDir
+make install INSTALL_ROOT=${STAGE_DIR}/AppDir
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage -O ${PROJECT_DIR}/build-tools/linuxdeploy-x86_64.AppImage
 wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage -O ${PROJECT_DIR}/build-tool/linuxdeploy-plugin-qt-x86_64.AppImage
 chmod +x ${PROJECT_DIR}/build-tools/*.AppImage
 export QMAKE=$(which qmake6)
-${PROJECT_DIR}/build-tools/linuxdeploy-x86_64.AppImage --appdir ${PROJECT_DIR}/AppDir -e "$PROJECT_DIR"/assets/icons/safe-exam-browser.png -d "$PROJECT_DIR"/packaging/linux/safe-exam-browser.desktop --output appimage --plugin qt
-
-# TODO: Figure out dynamic name of the built package --> LDAI_OUTPUT=filename
-# Move built package
-mv ${PROJECT_DIR}/Safe_Exam_Browser-x86_64.AppImage ${ARTIFACT_DIR}/${PACKAGE_NAME}-qt-x86_64.AppImage
+LDAP_OUTPUT=${ARTIFACT_DIR}/${PACKAGE_NAME}-qt-x86_64.AppImage ${PROJECT_DIR}/build-tools/linuxdeploy-x86_64.AppImage --appdir ${STAGE_DIR}/AppDir -e "$PROJECT_DIR"/assets/icons/safe-exam-browser.png -d "$PROJECT_DIR"/packaging/linux/safe-exam-browser.desktop --output appimage --plugin qt
 
 # Cleanup
-rm -rf ${PROJECT_DIR}/AppDir
+rm -rf ${STAGE_DIR}/AppDir
 rm -rf ${PROJECT_DIR}/bin/safe-exam-browser
 rm ${PROJECT_DIR}/Makefile
-
-# qmake6 CONFIG+=force_webkitgtk INSTALL_ROOT=AppDir
-# LD_DEBUG=all make -j$(nproc)
-# LD_DEBUG=all make install INSTALL_ROOT=AppDir
-# ./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage --plugin qt
-
-# # Move built package
-# mv ./Safe_Exam_Browser-x86_64.AppImage ${ARTIFACT_DIR}/${PACKAGE_NAME}-gtk-x86_64.AppImage
-
-# # Cleanup
-# rm -rf ./AppDir
-# rm -rf ./bin/safe-exam-browser
-# rm ./Makefile
 
 # Build for debian (outdated)
 # mkdir -p "${ARTIFACT_DIR}/debian/DEBIAN"
