11169e69060e4e6179b0445a83e7ccb35c4c7993
tectrixdev <joranhennion@duck.com>
2026-05-01 13:18:30 +0200

    Manually add all missing dependencies.

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -30,7 +30,8 @@ if command -v apt >/dev/null; then
     libssl3 \
     libtiff-dev \
     libxcb-cursor0 \
-    libxcb-xinput0
+    libxcb-xinput0 \
+    libnghttp3-9
 else
     echo "detected non-debian system, skipping dependency install"
 fi
